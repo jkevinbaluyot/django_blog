a601a1dc1012dc81e103548b6525fcc8729b21e8
John Kevin Baluyot <jkevinbaluyot@gmail.com>
2025-05-08 19:41:19 +0800

    Added tables

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'John Doe',
-        'title': 'Blog Post 1',
-        'content': 'Content of blog post 1',
-        'date_posted': 'August 27, 2023'
-    },
-    {
-        'author': 'Jane Smith',
-        'title': 'Blog Post 2',
-        'content': 'Content of blog post 2',
-        'date_posted': 'August 28, 2023'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
         'title': 'Blog List'
     }
     return render(request, 'blog/home.html', context)
