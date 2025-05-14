52292da25b8ba23d5d4495afa8b48fb9ec51f4f8
John Kevin Baluyot <jkevinbaluyot@gmail.com>
2025-05-14 20:29:20 +0800

    User login

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField()
@@ -12,4 +13,21 @@ class UserRegistrationForm(UserCreationForm):
             'email', 
             'password1', 
             'password2'
+        ]
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = [
+            'username', 
+            'email',
+        ]
+class ProfileUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = Profile
+        fields = [
+            'image'
         ]
